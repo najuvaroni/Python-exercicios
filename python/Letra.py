5e4344a76c9544e59a3eccf8702925c0cc8d8436
najuvaroni <124930475+najuvaroni@users.noreply.github.com>
2023-03-11 16:49:53 -0300

    Add files via upload

---
+++ python/Letra.py
@@ -0,0 +1,7 @@
+letra = input ("Informe a Letra: ")
+
+if(letra == "a" or letra == "e" or letra == "i" or letra ==  "o" or letra ==  "u"):
+    print("Sua letra é uma avogal"  )
+
+else:
+    print(" Sua letra é uma Consoante " )
---
+++ python/MaiorMenor.py
@@ -0,0 +1,13 @@
+print("Olá, mundo!")
+
+num = float(input("Informe o número: ") )
+  
+if(num > 0):
+    print("Positivo " + str(num) )
+
+elif(num < 0):
+    print("Negativo" + str(num))
+
+else:
+    print("zero " + str(num))
+
